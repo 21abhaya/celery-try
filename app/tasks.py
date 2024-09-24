17f0ee1ccff248bf03955f57db6dbefcb7ea1646
abhaya <gautamabhaya98@gmail.com>
2024-09-24 16:14:19 +0545

    migrations added

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-
+from .models import Widget
 
 @shared_task
 def add(x, y):
@@ -14,3 +14,19 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
+
+
+@shared_task
+def count_widgets():
+    return Widget.objects.count()
+
+
+@shared_task
+def rename_widget(widget_id, name):
+    w = Widget.objects.get(id=widget_id)
+    w.name = name
+    w.save()
