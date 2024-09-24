21b7427af83defe6153dfbfe3ad541f464370643
abhaya <gautamabhaya98@gmail.com>
2024-09-24 15:23:57 +0545

    added celery

--- sample/celery.py
+++ sample/celery.py
@@ -4,6 +4,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample.settings')
 
+#Used 'cel' instead of 'app', like in the documentation, to avoid any confusion since this project already has a module named 'app'.
 cel = Celery('sample')
 
 # the configuration object to child processes.
