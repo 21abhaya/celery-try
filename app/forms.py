18110398b5c33ad4a9affb8d98556a035ecdd202
abhaya <gautamabhaya98@gmail.com>
2024-09-25 13:21:53 +0545

    added form validation

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,17 @@
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 from .models import FileConversion
 
 class FileConversionForm(ModelForm):
-    
+    def clean_original_file_type(self):
+        data = self.cleaned_data['original_file_type']
+        if data not in ['PDF', 'JPG', 'PNG', 'SVG']:
+            raise ValidationError("Invalid file type")
+        return data
     
     class Meta:
         model = FileConversion
         fields = ['original_file', 'original_file_type', 'conversion_type',]
         help_texts = {
             'original_file': 'Upload a file to convert',}
+        
