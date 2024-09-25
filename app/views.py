39fe49a45292209eadaadb12688828759b6e1fae
abhaya <gautamabhaya98@gmail.com>
2024-09-25 17:35:20 +0545

    view function needs changes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import FileConversion
+from .forms import FileConversionForm
+
+def file_upload(request):
+    if request.method == 'POST':
+        
+        form = FileConversionForm(request.POST, request.FILES)
+
+        if form.is_valid():        
+            
+            handle_file_conversion(request.FILES['file'])
+            return HttpResponse("File uploaded successfully.")
+        else:
+            return HttpResponse("Invalid form data.")
+        
+    else:
+        form = FileConversionForm()
+        
+        return render(request, 'file_upload.html', {'form': form})
+
