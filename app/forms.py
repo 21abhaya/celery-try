8462f770414610bb3de265cce0e4b4ccd91c35e5
abhaya <gautamabhaya98@gmail.com>
2024-09-25 12:55:53 +0545

    added form

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import FileConversion
+
+class FileConversionForm(ModelForm):
+    class Meta:
+        model = FileConversion
+        fields = ['original_file', 'conversion_type', 'original_file_type',]
+        
