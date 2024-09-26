12a7b46ecba797b273bd92223a9934ee0af61894
abhaya <gautamabhaya98@gmail.com>
2024-09-26 23:01:58 +0545

    view needs functional changes asap

--- app/views.py
+++ app/views.py
@@ -8,16 +8,5 @@ def file_upload(request):
     if request.method == 'POST':
         
         form = FileConversionForm(request.POST, request.FILES)
-
-        if form.is_valid():        
-            
-            handle_file_conversion(request.FILES['file'])
-            return HttpResponse("File uploaded successfully.")
-        else:
-            return HttpResponse("Invalid form data.")
         
-    else:
-        form = FileConversionForm()
-        
-        return render(request, 'file_upload.html', {'form': form})
 
