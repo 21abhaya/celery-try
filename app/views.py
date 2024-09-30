18c58d1b9b1c4d55edea0624618f1a4a7cfbe0c2
abhaya <gautamabhaya98@gmail.com>
2024-10-01 00:59:40 +0545

    testing view and template

--- app/views.py
+++ app/views.py
@@ -8,6 +8,9 @@ def file_upload(request):
     if request.method == 'POST':
         
         form = FileConversionForm(request.POST, request.FILES)
-        
-        return 
+        form.save()
 
+        return render(request, 'app/base.html', {'form': form})
+    else:
+        form = FileConversionForm()
+        return render(request, 'app/base.html', {'form': form})
