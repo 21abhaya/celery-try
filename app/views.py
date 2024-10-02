4e6b1d55ed501d8c90d4f3a3d589d6bb37818fa5
abhaya <gautamabhaya98@gmail.com>
2024-10-03 01:56:01 +0545

    view logic needs changes

--- app/views.py
+++ app/views.py
@@ -4,11 +4,27 @@ from django.http import HttpResponse
 from .models import FileConversion
 from .forms import FileConversionForm
 
+import magic
+
 def file_upload(request):
     if request.method == 'POST':
         
         form = FileConversionForm(request.POST, request.FILES)
-        form.save()
+        if form.is_valid():
+            uploaded_file = form.cleaned_data['file']
+
+            # File type detection
+            mime = magic.Magic(mime=True)
+            file_format = mime.from_buffer(uploaded_file.read(1024))
+
+            #Reset file pointer
+            uploaded_file.seek(0)
+
+            conversion_request = FileConversion.objects.create(
+                original_file = uploaded_file,
+                original_file_format = file_format,
+                conversion_type = form.cleaned_data['conversion_type'],
+            )
 
         return render(request, 'app/base.html', {'form': form})
     else:
