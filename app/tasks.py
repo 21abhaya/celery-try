83c7b47d63ce0335f5758caf841dec6ba52300e2
abhaya <gautamabhaya98@gmail.com>
2024-10-10 23:02:30 +0545

    test commit

--- app/tasks.py
+++ app/tasks.py
@@ -1,1 +1,8 @@
 from celery import shared_task
+from reportlab.pdfgen import canvas
+from PIL import Image
+
+@shared_task 
+def convert_to_pdf(file_path, output_path):
+    canvas = canvas.Canvas(output_path)
+    canvas.drawImage(file_path, 0, 0)
