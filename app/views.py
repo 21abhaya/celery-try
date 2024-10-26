c38edbb74d457c8f3b595f568e636e003a432114
abhaya <gautamabhaya98@gmail.com>
2024-10-26 23:14:01 +0545

    work: added celery task in view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import FileConversion
 from .forms import FileConversionForm
+from .tasks import convert_to_pdf
 
 import magic
 
@@ -21,17 +21,33 @@ def file_upload(request):
             uploaded_file.seek(0)
 
             conversion_request = FileConversion.objects.create(
-                original_file = uploaded_file,
-                original_file_format = file_format,
-                conversion_type = form.cleaned_data['conversion_type'],
+                original_file=uploaded_file,
+                original_file_format=file_format,
+                conversion_type=form.cleaned_data['conversion_type'],
             )
+            
+            # validate the model
+            conversion_request.full_clean()
+            conversion_request.save()
 
+            # generate output pdf path
+            output_pdf = f'media/converted_files/{uploaded_file.name.split(".")[0]}.pdf'
+
+            # launch  celery task
+            task = convert_to_pdf.delay(
+                conversion_request.original_file.path,
+                output_pdf
+            )
+            # store task ID in session to check status
+            request.session['task_id'] = task.id
+            
             context = {
                 'form': form,
                 'conversion_type': FileConversion.CONVERSION_TYPES,
+                'task_id': task.id,
             }
 
-        return render(request, 'app/base.html', {'form': form, 'conversion_type': FileConversion.CONVERSION_TYPES})
+        return render(request, 'app/base.html', context)
     else:
         form = FileConversionForm()
-        return render(request, 'app/base.html', {'form': form, 'conversion_type': FileConversion.CONVERSION_TYPES})
+        return render(request, 'app/base.html', context)
