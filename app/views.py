1813fb6682004ecf5d61a808e68d7ee0163f2c56
abhaya <gautamabhaya98@gmail.com>
2024-11-02 18:41:27 +0545

    added: mime types

--- app/views.py
+++ app/views.py
@@ -38,7 +38,15 @@ def file_upload(request):
                             'application/vnd.ms-excel',
                             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
                             'application/vnd.ms-powerpoint',
-                            'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+                            'application/vnd.openxmlformats-officedocument.presentationml.presentation','image/jpeg',
+                            'image/png',
+                            'image/gif',
+                            'image/svg+xml',
+                            'image/tiff',
+                            'application/zip',
+                            'application/x-rar-compressed',
+                            'application/x-tar',
+                            'application/gzip'
                         ] 
             if file_format in mime_types:
                 # launch  celery task
